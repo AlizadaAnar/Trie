b74fb9dd01ea6881977d48ce53fe8e5c5b9beb42
anaralizada <anar_alizade@internet.ru>
2024-06-01 17:11:46 +0400

    adding new fields for sharing

--- src/main/java/com/company/MyFarm/sharing/SharingService.java
+++ src/main/java/com/company/MyFarm/sharing/SharingService.java
@@ -2,7 +2,6 @@ package com.company.MyFarm.sharing;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
